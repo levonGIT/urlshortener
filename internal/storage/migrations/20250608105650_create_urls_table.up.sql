cfe505765c66b9a5ea9d7f8d58d3b3a79ded64c0
Levon <akopyanlevon17@gmail.com>
2025-06-09 23:44:11 +0500

    added index migration for alias, updated max-length for alias

--- internal/storage/migrations/20250608105650_create_urls_table.up.sql
+++ internal/storage/migrations/20250608105650_create_urls_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE urls (
-    id integer PRIMARY KEY,
-    alias TEXT NOT NULL UNIQUE,
+    id SERIAL PRIMARY KEY,
+    alias varchar(255) NOT NULL UNIQUE,
     url TEXT NOT NULL,
     count integer NOT NULL DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
